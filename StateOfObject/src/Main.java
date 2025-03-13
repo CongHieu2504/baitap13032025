563a1db9a6212a0d59839e8966e28faba12788b6
Cong Hieu <hieuho250403@gmail.com>
2025-03-13 07:52:57 +0700

    CO Phieu

---
+++ StateOfObject/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Stock SamsungStock = new Stock("Samsung", 500);
+        Investor investor1 = new Investor("Huy");
+        Investor investor2 = new Investor("Anh");
+        Investor investor3 = new Investor("Hieu");
+
+        SamsungStock.addObserver(investor1);
+        SamsungStock.addObserver(investor2);
+        SamsungStock.addObserver(investor3);
+
+        System.out.println("price changed 1");
+        SamsungStock.setPrice(550);
+
+        System.out.println("price changed 2");
+        SamsungStock.setPrice(600);
+
+
+    }
+}
---
+++ StateOfObject/src/StockObserver.java
@@ -0,0 +1,3 @@
+public interface StockObserver {
+    public void update(String stockName , double price);
+}
